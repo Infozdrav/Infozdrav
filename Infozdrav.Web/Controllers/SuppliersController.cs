070f8fddffe9f40ff196578a7c0b7863bc9afde5
EVA BONEŠ <eb1690@student.uni-lj.si>
2018-04-10 16:00:13 +0200

    preverjam

---
+++ Infozdrav.Web/Controllers/SuppliersController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Infozdrav.Web.Data;
+using Infozdrav.Web.Models.Manage;
+using Infozdrav.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Infozdrav.Web.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        
+    }
+}
