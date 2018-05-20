8762f252a31db2f99f0fd38d89db5aa355c07b14
Sebastjan Kozoglav <sk5429@student.uni-lj.si>
2018-05-20 17:04:11 +0200

    Added Obdelava vzorcev section

--- Infozdrav.Web/Controllers/ObdelavaVzorcevController.cs
+++ Infozdrav.Web/Controllers/ObdelavaVzorcevController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infozdrav.Web.Models.Labena;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Infozdrav.Web.Controllers
@@ -9,6 +10,26 @@ namespace Infozdrav.Web.Controllers
     public class ObdelavaVzorcevController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IzberiVzorec()
+        {
+            return View();
+        }
+
+        public IActionResult Obdelava()
+        {
+            return View(new ObdelavaVzorcevViewModel());
+        }
+
+        public IActionResult Rezultati()
+        {
+            return View();
+        }
+
+        public IActionResult VzorciVObdelavi()
         {
             return View();
         }
--- Infozdrav.Web/Models/Labena/ObdelavaVzorcevViewModel.cs
+++ Infozdrav.Web/Models/Labena/ObdelavaVzorcevViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,23 @@ namespace Infozdrav.Web.Models.Labena
 {
     public class ObdelavaVzorcevViewModel
     {
+        [Required]
+        public DateTime? DatumObdelave { get; set; }
+
+        [Required]
+        public string Oseba { get; set; }
+
+        [Required]
+        public string Izolat { get; set; }
+
+        [Required]
+        public string Protokol { get; set; }
+
+        [Required]
+        public string Aparatura { get; set; }
+
+        [Required]
+        public string Kemikalije { get; set; }
+
     }
 }
