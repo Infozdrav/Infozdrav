3a41c2f7915ee9234191bf22226f6c96577b9c0e
Jaka Konda <jaka@konda.si>
2018-05-26 13:28:30 +0200

    Test and fixes.

--- Infozdrav.Web/wwwroot/js/site.js
+++ Infozdrav.Web/wwwroot/js/site.js
@@ -11,7 +11,9 @@ function HandleBarcodeResult(data) {
     var result = {
         "raw": data
     }
-    if (data.startsWith("R1")) { // Siements
+    if (data.startsWith("R1")) { // Siemens
+        result.type = "Siemens";
+
         // date
         var startOfDate = data.indexOf("?") + 1;
         var date = data.substring(startOfDate, startOfDate + 8);
@@ -22,18 +24,22 @@ function HandleBarcodeResult(data) {
         result.lot = data.substring(startOfLot, startOfDate - 1);
     }
     else if (data.charCodeAt(0) === 29 &&
-             data.indexOf("FN") !== -1) { // TotalPSA
+        data.indexOf("FN") !== -1) { // TotalPSA
+        result.type = "TotalPSA";
+
         // date
         var date = data.substring(19, 19 + 6);
-        result.data = new Date(date.substring(0, 2) * 1 + 2000, date.substring(2, 4) - 1, date.substring(4, 6));
+        result.date = new Date(date.substring(0, 2) * 1 + 2000, date.substring(2, 4) - 1, date.substring(4, 6));
 
         result.lot = data.substring(27, 27 + 9); // lot
         result.gtin = data.substring(3, 3 + 14); // gtin
     }
     else if (data.charCodeAt(0) === 29) { // Roche
+        result.type = "Roche";
+
         // date
         var date = data.substring(30, 30 + 6); 
-        result.data = new Date(date.substring(0, 2)*1 + 2000, date.substring(2, 4) - 1, date.substring(4, 6));
+        result.date = new Date(date.substring(0, 2)*1 + 2000, date.substring(2, 4) - 1, date.substring(4, 6));
 
         result.lot = data.substring(19, 19 + 8); // lot
         result.gtin = data.substring(3, 3 + 15); // gtin
@@ -43,9 +49,16 @@ function HandleBarcodeResult(data) {
         barcodeResultCallback(result);
 }
 
-function scanBarcode(callback) {
-    barcodeResultCallback = callback;
+function scanBarcode(callbackName) {
+    barcodeResultCallback = window[callbackName];
     var scanner = window.open("/Scanner/Camera",
         "_blank",
         "height=480,width=640,toolbar=no,status=no,scrollbars=no,menubar=no,location=no,toolbar=no");
 }
+
+
+$(document).ready(function() {
+    $(".barcode-scanner").click(function() {
+        scanBarcode($(this).attr("data-callback"));
+    });
+})
